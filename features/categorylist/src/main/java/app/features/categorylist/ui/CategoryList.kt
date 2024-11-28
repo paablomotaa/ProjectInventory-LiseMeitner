d67add8469458ae6fb880cf3a003334f8ef3cd91
FJVidalG <fjvidalgarcia@gmail.com>
2024-11-28 10:53:28 +0100

    Update CategoryList.kt

--- features/categorylist/src/main/java/app/features/categorylist/ui/CategoryList.kt
+++ features/categorylist/src/main/java/app/features/categorylist/ui/CategoryList.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Card
 import androidx.compose.material3.FloatingActionButton
@@ -83,7 +84,7 @@ fun CategoryItem(category: Category, onClick: () -> Unit) {
                 )
             }
             Icon(
-                imageVector = androidx.compose.material.icons.Icons.Default.MoreVert, // Ícono de tres puntos
+                imageVector = androidx.compose.material.icons.Icons.Default.Edit,
                 contentDescription = "Opciones",
                 modifier = Modifier
                     .size(24.dp)
