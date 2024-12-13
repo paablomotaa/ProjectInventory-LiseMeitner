952ace45cb75cb54d591e8ec7cb3e4b6214744a4
Pablo Mota <69493409+paablomotaa@users.noreply.github.com>
2024-12-13 10:52:15 +0100

    Borro mi base result

--- base/utils/src/main/java/app/base/utils/BaseResult.kt
+++
@@ -1,6 +0,0 @@
-package app.base.utils
-
-sealed class BaseResult <out T>{
-    data class Success<T>(var data: T): BaseResult<T>()
-    data class Error(val exception: Exception): BaseResult<Nothing>()
-}
